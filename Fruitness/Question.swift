22ddc20d8656315ddb1bff1e885834d915675f65
izzatjabali <izyjab@gmail.com>
2020-09-28 23:41:15 +1000

    Change of approach on initial idea. Removed all images and Image Views. Buttons with emoji"s to substitute for the Images. A Collection Outlet linking all the different fruit Images. Cleaner and more synced understood code in the works.

---
+++ Fruitness/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Fruitness
+//
+//  Created by Izzat Jabali on 26/9/20.
+//
+
+import Foundation
+
+struct Question {
+    let text: [String]
+    let answer: [String]
+   
+}
--- Fruitness/ViewController.swift
+++ Fruitness/ViewController.swift
@@ -4,16 +4,145 @@
 //
 //  Created by Izzat Jabali on 26/9/20.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+{
+    
+    
+    var fruitCount = 0 {
+        didSet {
+            fruitLabel.text = "SCORE: \(fruitCount)"
+        }
     }
+    
+    
+    
 
+    @IBOutlet weak var fruitLabel: UILabel!
+    @IBOutlet var fruitButtons: [UIButton]!
+    @IBOutlet weak var scoreLabel: UILabel!
 
+    
+    
+    var fruitChoices = ["🍎","🍐","🍊","🍓","🍉","🍇","🍌","🍒"]
+    
+ 
+    @IBAction func optionButton1(_ sender: UIButton) {
+        fruitCount += 1
+        if let fruitNumber = fruitButtons.firstIndex(of: sender) {
+            print("fruitNumber = \(fruitNumber)")
+           // answerButtonPressed(withEmoji: "🍎", on: sender)
+        } else {
+            print("Wrong image choosen")
+        }
+        
+        
+    
+        
+        
+        
+     
+    }
+    
+    func answerButtonPressed(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+        } else {
+            button.setTitle("", for: UIControl.State.normal)
+        }
+    }
 }
+
+//}
+//    var fruitNames =
+//
+//        Question(text: [String], answer: [String])
+//        Question(text: "Pear", answer: "A"),
+//            Question(text: "Carrot", answer: "B"),
+//            Question(text: "Apple", answer: "C"),
+//            Question(text: "Strawberry", answer: "D"),
+//            Question(text: "Grapes", answer: "E"),
+//            Question(text: "Watermelon", answer: "F")
+//    }
+//
+//
+//
+//    //Variable to track which Questiin we are on.
+//    var fruitNumber = 0
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        updateUI()
+//    }
+//
+//    @IBAction func optionButtonPressed(_ sender: UIButton) {
+//
+//        var userAnswer = sender.currentTitle // True/False
+//        let actualQuestion = fruitNames[fruitNumber]
+//        let actualAnswer = actualQuestion.answer
+//
+//
+//
+//        switch fruitNames  {
+//        case "A":
+//            print("Pear")
+//        case "B":
+//            print("Carrot")
+//        case "C":
+//            print("Apple")
+//        case "D":
+//            print("Strawberry")
+//        case "E":
+//            print("Grapes")
+//        case "F":
+//            print("Apple")
+//        case "G":
+//            print("Watermelon")
+//        case "H":
+//            print("")
+//        }
+//
+//
+//
+//        fruitNumber += 1 //
+//
+//
+//        if fruitNumber + 1 < 5 { //End of Game
+//            fruitNumber += 1
+//        } else {
+//
+//            //open end of screen to replay <- GOES HERE!!
+//        }
+//        updateUI()
+//
+//    }
+//    func updateUI() {
+//        fruitLabel.text = fruitNames[fruitNumber].text
+//    }
+//
+//
+//    func updateGameState() {
+//
+//
+//
+//    }
+/*
+ optionButton3.setTitle(word, for: .normal)
+ optionButton2?.addTarget(self, action: #selector(ViewController.buttonPressed(_:)), for: UIControl.Event.touchUpInside)
+ //            optionButton3?.addTarget(self, action: #selector(ViewController.buttonPressed(_:)), for: UIControl.Event.touchUpInside)
+ 
+ 
+ func animateText() -> NSAttributedString {
+ let attachment:NSTextAttachment = NSTextAttachment()
+ attachment.image = UIImage(named: "launch.png")
+ attachment.setImageHeight(height: 20)
+ let attachmentString:NSAttributedString = NSAttributedString(attachment: attachment)
+ let attributedString:NSMutableAttributedString = NSMutableAttributedString(string:"This is the attributed String.")
+ attributedString.append(attachmentString)
+ return attributedString
+ }
+ */
 
